913015b299e631eec85590919665add7347d81a4
felipesanderp <felipe.panisson@hotmail.com>
2020-06-19 20:42:52 -0300

    add service and tets to ListMonthAvailability

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,8 +1,12 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 import ICreatAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreatAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO
+  ): Promise<Appointment[]>;
 }
